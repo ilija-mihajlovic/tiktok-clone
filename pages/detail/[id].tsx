04c34ea35415487607ecf584df996af560eaf621
Ilija Mihajlovic <iilija879a@gmail.com>
2022-09-12 22:33:44 +0200

    error fix

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,81 +1,84 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { useRouter } from 'next/router'
-import Image from 'next/image'
-import Link from 'next/link'
-import { GoVerified } from 'react-icons/go'
-import { MdOutlineCancel } from 'react-icons/md'
-import { BsFillPlayFill } from 'react-icons/bs'
-import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
-import axios from 'axios'
+import React, { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
+import { GoVerified } from 'react-icons/go';
+import Image from 'next/image';
+import Link from 'next/link';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 
-import { BASE_URL } from '../../utils'
-import { Video } from '../../types'
-import useAuthStore from '../../store/authStore'
-import LikeButton from '../../components/LikeButton'
-import Comments from '../../components/Comments'
+import Comments from '../../components/Comments';
+import { BASE_URL } from '../../utils';
+import LikeButton from '../../components/LikeButton';
+import useAuthStore from '../../store/authStore';
+import { Video } from '../../types';
+import axios from 'axios';
 
 interface IProps {
-    postDetails: Video
+  postDetails: Video;
 }
 
 const Detail = ({ postDetails }: IProps) => {
-  const [post, setPost] = useState(postDetails)
-  const [playing, setPlaying] = useState(false)
-  const [isVideoMuted, setIsVideoMuted] = useState(false)
-  const router = useRouter()
-  const videoRef = useRef<HTMLVideoElement>(null)
-  const { userProfile }: any = useAuthStore()
-  const [comment, setComment] = useState('')
-  const [isPostingComment, setIsPostingComment] = useState(false)
+  const [post, setPost] = useState(postDetails);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isVideoMuted, setIsVideoMuted] = useState<boolean>(false);
+  const [isPostingComment, setIsPostingComment] = useState<boolean>(false);
+  const [comment, setComment] = useState<string>('');
 
-  if(!post) return null
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
+
+  const { userProfile }: any = useAuthStore();
 
   const onVideoClick = () => {
-    if(playing){
-        videoRef?.current?.pause();
-        setPlaying(false)
-    } else{
-        videoRef?.current?.play();
-        setPlaying(true)
+    if (isPlaying) {
+      videoRef?.current?.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setIsPlaying(true);
     }
-  }
+  };
 
   useEffect(() => {
-    if(post && videoRef?.current){
-        videoRef.current.muted = isVideoMuted
+    if (post && videoRef?.current) {
+      videoRef.current.muted = isVideoMuted;
     }
-  }, [post, isVideoMuted]) 
+  }, [post, isVideoMuted]);
 
-  const handleLike = async (like:boolean) => {
-    if(userProfile){
-        const { data } = await axios.put(`${BASE_URL}/api/like`, {
-            userId: userProfile._id,
-            postId: post._id,
-            like
-        })
-        setPost({ ...post, likes:data.likes })
-    }
-  }
-
-  const addComment = async (e) => {
-    e.preventDefault()
-
-    if(userProfile && comment){
-      setIsPostingComment(true)
-
-      const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+  const handleLike = async (like: boolean) => {
+    if (userProfile) {
+      const res = await axios.put(`${BASE_URL}/api/like`, {
         userId: userProfile._id,
-        comment
-      })
-
-      setPost({ ...post, comments:data.comments })
-      setComment('')
-      setIsPostingComment(false)
+        postId: post._id,
+        like
+      });
+      setPost({ ...post, likes: res.data.likes });
     }
-  }
+  };
+
+  const addComment = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (userProfile) {
+      if (comment) {
+        setIsPostingComment(true);
+        const res = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+          userId: userProfile._id,
+          comment,
+        });
+
+        setPost({ ...post, comments: res.data.comments });
+        setComment('');
+        setIsPostingComment(false);
+      }
+    }
+  };
 
   return (
-    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+    <>
+      {post && (
+        <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
           <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
             <div className='opacity-90 absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
               <p className='cursor-pointer ' onClick={() => router.back()}>
@@ -89,12 +92,12 @@ const Detail = ({ postDetails }: IProps) => {
                   onClick={onVideoClick}
                   loop
                   src={post?.video?.asset.url}
-                  className='h-full cursor-pointer'
+                  className=' h-full cursor-pointer'
                 ></video>
               </div>
 
               <div className='absolute top-[45%] left-[40%]  cursor-pointer'>
-                {!playing && (
+                {!isPlaying && (
                   <button onClick={onVideoClick}>
                     <BsFillPlayFill className='text-white text-6xl lg:text-8xl' />
                   </button>
@@ -125,11 +128,11 @@ const Detail = ({ postDetails }: IProps) => {
                     src={post.postedBy.image}
                   />
                   <div>
-                    <div className='text-xl font-bold tracking-wider flex gap-2 items-center justify-center'>
-                      {post.postedBy.userName}{' '}
+                    <div className='text-xl font-bold lowercase tracking-wider flex gap-2 items-center justify-center'>
+                      {post.postedBy.userName.replace(/\s+/g, '')}{' '}
                       <GoVerified className='text-blue-400 text-xl' />
                     </div>
-                    <p className='text-md'>{post.postedBy.userName}</p>
+                    <p className='text-md'> {post.postedBy.userName}</p>
                   </div>
                 </div>
               </Link>
@@ -137,38 +140,38 @@ const Detail = ({ postDetails }: IProps) => {
                 <p className=' text-md text-gray-600'>{post.caption}</p>
               </div>
               <div className='mt-10 px-10'>
-                        {userProfile && <LikeButton
-                            likes={post.likes}
-                            flex='flex'
-                            handleLike={() => handleLike(true)}
-                            handleDislike={() => handleLike(false)}
-                        />}
-                    </div>
-
-                    <Comments 
-                      comment = {comment}
-                      setComment = {setComment}
-                      addComment = {addComment}
-                      comments = {post.comments}
-                      isPostingComment = {isPostingComment}
-                    />
-
-                </div>
+                {userProfile && <LikeButton
+                  likes={post.likes}
+                  flex='flex'
+                  handleLike={() => handleLike(true)}
+                  handleDislike={() => handleLike(false)}
+                />}
+              </div>
+              <Comments
+                comment={comment}
+                setComment={setComment}
+                addComment={addComment}
+                comments={post.comments}
+                isPostingComment={isPostingComment}
+              />
             </div>
-    </div>          
-  )
-}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 export const getServerSideProps = async ({
-    params: { id }
-} : {
-    params: { id: string }
+  params: { id },
+}: {
+  params: { id: string };
 }) => {
-   const { data } = await axios.get(`${BASE_URL}/api/post/${id}`)
+  const res = await axios.get(`${BASE_URL}/api/post/${id}`);
 
-   return{
-    props: { postDetails: data }
-   }
-}
+  return {
+    props: { postDetails: res.data },
+  };
+};
 
-export default Detail
+export default Detail;
